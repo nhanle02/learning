6983ac99d19b03dd717bfbd236fb89aa0bbec4f5
lenghiadev <nghia.dev95@gmail.com>
2022-08-09 20:23:46 +0700

    Add slide top

--- templetes/js/main.js
+++ templetes/js/main.js
@@ -2,4 +2,30 @@ $(function() {
     $('.js-click').on('click', function() {
         $('.dropdown').slideToggle();
     });
+
+    $('.js-slick-slide').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        responsive: [
+          {
+            breakpoint: 768,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              arrows: false,
+              centerMode: true,
+              centerPadding: '40px',
+              slidesToShow: 1
+            }
+          }
+        ]
+      });
 });
