d356b40f021b4ac6dac23560177e6244ab4f7778
lenghiadev <nghia.dev95@gmail.com>
2022-08-12 22:03:06 +0700

    Learning D3

---
+++ PHP/D3/baitap.php
@@ -0,0 +1,73 @@
+1. Viết code PHP để lấy thông tin cấu hình và phiên bản của PHP
+
+2. Viết code PHP để in một chuỗi cho trước
+
+3. Viết code PHP để tạo một form đơn giản để nhận và hiển thị tên đăng nhập
+
+4. Lấy địa chỉ Client IP trong PHP
+
+5. Viết PHP script để lấy tên file hiện tại
+
+6. Lấy URL trong PHP
+
+7. Đổi màu kí tự đầu tiên của từ trong PHP
+
+8. Viết PHP script để kiểm tra xem trang là được gọi từ https hay http
+
+#Redirect trong PHP
+
+9. Email Validation trong PHP
+
+# Viết PHP script để lấy source code của một trang web (curl)
+
+# Viết PHP script để lấy thông tin chỉnh sửa lần cuối cùng (Last Modified) của một file
+
+10. Viết PHP script để đếm số dòng trong một file
+
+11. In phiên bản PHP hiện tại
+
+# Viết PHP script để dừng thực thi chương trình trong một khoảng thời gian đã cho
+
+12. Sử dụng toán tử tam phân kiểm tra một số là lớn hơn 10, 15 hay 20
+
+13. Viết PHP script để lấy thư mục gốc
+
+14. Lấy thông tin Hệ điều hành mà PHP đang chạy trên
+
+# Viết PHP script để lấy thời gian chỉnh sửa lần cuối cùng của trang hiện tại
+
+15. Hiển thị tất cả các số nằm trong khoảng (200, 250) mà chia hết cho 4
+
+16. Viết PHP script để tạo mảng liên hợp hiển thị tên các quốc gia và thủ đô
+
+17. Xóa phần tử mảng trong PHP
+
+18. Viết PHP script để lấy phần tử đầu tiên trong mảng
+
+19. Viết PHP script để chèn phần tử vào mảng
+
+20. Xử lý JSON string trong PHP
+
+21. Sắp xếp mảng liên hợp trong PHP
+
+22. Viết PHP script để tìm giá trị trung bình của các phần tử mảng và hiển thị 5 phần tử nhỏ nhất và lớn nhất
+
+23. Chuyển đổi mảng thành chữ hoa - chữ thường
+
+24. Tìm độ dài ngắn/dài nhất của các phần tử (dạng chuỗi) trong mảng
+
+25. Tìm key lớn nhất trong mảng PHP
+
+26. Tìm giá trị nhỏ nhất trong mảng PHP
+
+27. Sắp xếp mảng trong PHP (theo thứ tự tự nhiên và không phân biệt kiểu chữ)
+
+28. Sắp xếp mảng theo chiều đảo ngược (còn gọi là đảo mảng)
+
+29. Tìm giá trị trong một mảng liên hợp
+
+30. Xóa bản sao giá trị trong mảng
+
+31. Kiểm tra xem các giá trị trong mảng có phải là chuỗi không
+
+32. Tìm giá trị lớn nhất, nhỏ nhất của một tập hợp các mảng số nguyên
