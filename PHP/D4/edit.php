66631c9a586ae3b9e84f43e6f00bf8b0c306993d
lenghiadev <nghia.dev95@gmail.com>
2022-08-28 22:17:40 +0700

    Learning CRUD users

--- PHP/D4/edit.php
+++ PHP/D4/edit.php
@@ -2,8 +2,19 @@
     require "./common/header.php";
 ?>
     <div class="container mt-4">
-        <h4>Create Users</h4>
+        <h4>Edit Users</h4>
         <?php
+            if (empty($_GET['id'])) {
+                header('Location: index.php');
+            }
+
+            $sqlEdit = "SELECT * FROM users where id = ?";
+            $prepare = $conn->prepare($sqlEdit);
+            $prepare->execute([$_GET['id']]);
+            $prepare->setFetchMode(PDO::FETCH_ASSOC);
+            $user = $prepare->fetch();
+
+
             function isUniqueEmail($email) {
                 global $conn;
                 $sql = "SELECT id from users where email = ?";
@@ -52,35 +63,20 @@
                     ];
                     if (!in_array($avatar['type'], $types) ) {
                         $errors['avatar'] = 'File đã chọn chưa đúng định dạng';
-                    } else if ($avatar['size'] > 2048 * 1000) {
+                    } else if ($avatar['size'] > 2048) {
                         $errors['avatar'] = 'Vui lòng chọn file không quá 2MB';
                     }
                 }
 
                 if (empty($errors)) {
-                    if ($avatar['error'] === 0) {
-                        $pathSave = '../../PHP/D4/uploads/' . $avatar['name'];
-                        move_uploaded_file($avatar['tmp_name'], $pathSave);
-                    }
-                    $fileName = $avatar['name'];
-                    $password = password_hash($password, PASSWORD_BCRYPT);
-                    $sql = "INSERT INTO users (full_name, email, password, phone, address, status, avatar)
-                        VALUES (?, ?, ?, ?, ?, ?, ?)
-                    ";
-                    $stmt = $conn->prepare($sql);
-                    $result = $stmt->execute([$fullName, $email, $password, $phone, $address, $status, $fileName]);
-                    if (!empty($result)) {
-                        header('Location: index.php');
-                    } else {
-                        echo 'Đã có lỗi trong quá trình tạo mới users';
-                    }
+                    // Xử lý update
                 }
             }
         ?>
         <form action="" method="POST" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="">Full name</label>
-                <input type="text" name="full_name" value="<?php echo $fullName ?? '' ?>" class="form-control" />
+                <input type="text" name="full_name" value="<?php echo $fullName ?? $user['full_name'] ?>" class="form-control" />
                 <?php if (!empty($errors['full_name'])) { ?>
                     <span class="text-danger"><?php echo $errors['full_name']; ?></span>
                 <?php
@@ -89,7 +85,7 @@
             </div>
             <div class="form-group">
                 <label for="">Email</label>
-                <input type="text" name="email" value="" class="form-control" />
+                <input type="text" name="email" value="<?php echo $email ?? $user['email'] ?>" class="form-control" />
                 <?php if (!empty($errors['email'])) { ?>
                     <span class="text-danger"><?php echo $errors['email'] ?></span>
                 <?php } ?>
@@ -113,26 +109,29 @@
 
             <div class="form-group">
                 <label for="">Phone</label>
-                <input type="text" name="phone" value="" class="form-control" />
+                <input type="text" name="phone" value="<?php echo $phone ?? $user['phone'] ?>" class="form-control" />
             </div>
 
             <div class="form-group">
                 <label for="">Address</label>
-                <input type="text" name="address" value="" class="form-control" />
+                <input type="text" name="address" value="<?php echo $address ?? $user['address'] ?>" class="form-control" />
             </div>
 
             <div class="form-group">
                 <label for="">Avatar</label>
-                <input type="file" name="avatar" value="" class="form-control" />
+                <input type="file" name="avatar" value="<?php echo $avatar ?? $user['avatar'] ?>" class="form-control" />
                 <?php if (!empty($errors['avatar'])) { ?>
                     <span class="text-danger"><?php echo $errors['avatar'] ?></span>
                 <?php } ?>
+                <div class="preview-image">
+                    <img style="object-fit: contain;" width="200" height="200" src="../../PHP/D4/uploads/<? echo $user['avatar'] ?>" alt="" />
+                </div>
             </div>
 
             <div class="form-group">
                 <label for="">Status</label> <br>
-                Active:<input type="radio" name="status" value="1" />
-                Deactive<input type="radio" name="status" value="2" />
+                Active:<input type="radio" name="status" <?php echo ($status ?? $user['status']) == 1 ? 'checked' : '' ?> value="1" />
+                Deactive<input type="radio" name="status" <?php echo ($status ?? $user['status']) == 2 ? 'checked' : '' ?>  value="2" />
             </div>
 
             <div class="form-group">
--- PHP/D4/index.php
+++ PHP/D4/index.php
@@ -1,68 +1,70 @@
 <?php
     require "./common/header.php";
 ?>
-    <h4>Thực hiện chức năng CRUD với table users</h4>
-    <?php
-        $mysql = "SELECT * FROM users";
+    <div class="container mt-4">
+        <h4>Thực hiện chức năng CRUD với table users</h4>
+        <?php
+            $mysql = "SELECT * FROM users";
 
-        // Sử dụng Prepare 
-        $stmt = $conn->prepare($mysql);
+            // Sử dụng Prepare 
+            $stmt = $conn->prepare($mysql);
 
-        // Thực thi câu truy vấn
-        $stmt->execute();
+            // Thực thi câu truy vấn
+            $stmt->execute();
 
-        // Khai báo fetch kiểu mảng kết hợp
-        $stmt->setFetchMode(PDO::FETCH_ASSOC); 
+            // Khai báo fetch kiểu mảng kết hợp
+            $stmt->setFetchMode(PDO::FETCH_ASSOC); 
 
-        $results = $stmt->fetchAll();
-        // echo '<pre>';
-        // echo print_r($results, true);
-        // echo '</pre>'
-    ?>
-    <a href="create.php" class="btn btn-success mb-1">Create +</a>
-    <table class="table" style="width: 1000px;">
-        <thead>
-            <tr>
-                <th scope="col">ID</th>
-                <th scope="col">Full name</th>
-                <th scope="col">Email</th>
-                <th scope="col">Phone</th>
-                <th scope="col">Address</th>
-                <th scope="col">Status</th>
-                <th scope="col">Operation</th>
-            </tr>
-        </thead>
-        <tbody>
-            <?php 
-            if (!empty($results)) { 
-                foreach ($results as $result) {
-            ?>
+            $results = $stmt->fetchAll();
+            // echo '<pre>';
+            // echo print_r($results, true);
+            // echo '</pre>'
+        ?>
+        <a href="create.php" class="btn btn-success mb-1">Create +</a>
+        <table class="table" style="width: 1000px;">
+            <thead>
                 <tr>
-                    <td><?php echo $result['id'] ?></td>
-                    <td><?php echo $result['full_name'] ?></td>
-                    <td><?php echo $result['email'] ?></td>
-                    <td><?php echo $result['phone'] ?></td>
-                    <td><?php echo $result['address'] ?></td>
-                    <td>
-                        <?php
-                            $status = '<span class="badge badge-success">Kich hoat</span>';
-                            if ((int) $result['status'] === 2) {
-                                $status = '<span class="badge badge-danger">Chua Kich hoat</span>';
-                            }
-                            echo $status;
-                        ?>
-                    </td>
-                    <td>
-                        <a href="" class="btn btn-info">Edit</a>
-                        <a href="" class="btn btn-danger">Delete</a>
-                    </td>
+                    <th scope="col">ID</th>
+                    <th scope="col">Full name</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Status</th>
+                    <th scope="col">Operation</th>
                 </tr>
-            <?php 
-                }
-            } 
-            ?>
-        </tbody>
-    </table>
+            </thead>
+            <tbody>
+                <?php 
+                if (!empty($results)) { 
+                    foreach ($results as $result) {
+                ?>
+                    <tr>
+                        <td><?php echo $result['id'] ?></td>
+                        <td><?php echo $result['full_name'] ?></td>
+                        <td><?php echo $result['email'] ?></td>
+                        <td><?php echo $result['phone'] ?></td>
+                        <td><?php echo $result['address'] ?></td>
+                        <td>
+                            <?php
+                                $status = '<span class="badge badge-success">Kich hoat</span>';
+                                if ((int) $result['status'] === 2) {
+                                    $status = '<span class="badge badge-danger">Chua Kich hoat</span>';
+                                }
+                                echo $status;
+                            ?>
+                        </td>
+                        <td>
+                            <a href="edit.php?id=<?php echo $result['id'] ?>" class="btn btn-info">Edit</a>
+                            <a href="" class="btn btn-danger">Delete</a>
+                        </td>
+                    </tr>
+                <?php 
+                    }
+                } 
+                ?>
+            </tbody>
+        </table>
+    </div>
 <?php
     require "./common/footer.php";
 ?>
