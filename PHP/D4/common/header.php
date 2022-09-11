3cec70d93e01bca813632e2d44e3064a1c85f3e6
nhanle02 <nhanle.020902@gmail.com>
2022-09-11 21:34:44 +0700

    add

--- PHP/D4/common/header.php
+++ PHP/D4/common/header.php
@@ -8,6 +8,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
     <title>Bai tap voi co so du lieu</title>
 </head>
     <body>
